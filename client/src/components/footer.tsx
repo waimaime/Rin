d4d361c3ea062aecb0c6e5b4b21f3f74130a1b04
waimaime <37923779+waimaime@users.noreply.github.com>
2026-09-07 20:03:16 +0800

    Update footer.tsx

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -102,7 +102,7 @@ function Footer() {
                             setDoubleClickTimes(doubleClickTimes + 1)
                         }
                     }}>
-                        © {new Date().getFullYear()} Powered by <a className='hover:underline' href="https://github.com/openRin/Rin" target="_blank">Rin</a>
+                        © {new Date().getFullYear()} Powered by <a className='hover:underline' href="https://kkkkm.com" target="_blank">KM</a>
                     </span>
                     {config.getBoolean('rss') && <>
                         <Spliter />
